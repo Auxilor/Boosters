f3bcc0492f46c8701e46e497b3c65a02a2bf17bb
NicoNeko <nicuch100@gmail.com>
2026-03-09 09:14:35 +0200

    Added %time% to reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
@@ -2,6 +2,8 @@ package com.willfp.boosters.commands
 
 import com.willfp.boosters.plugin
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.toNiceString
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(
@@ -12,7 +14,9 @@ object CommandReload : Subcommand(
 ) {
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        plugin.reload()
-        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+        sender.sendMessage(
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toNiceString())
+        )
     }
 }
